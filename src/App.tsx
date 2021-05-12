22190d32ce0fece9d9407e764f365b914a5a4eef
Eugene <egor_egorov_qwerty@mail.ru>
2021-05-13 01:46:04 +0300

    conquestTS

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import cn from 'classnames';
 
 import s from './App.module.scss';
+import './example'
 
 const App = () => {
-  console.log('asdasd');
-
   return <div className={cn(s.header, 'color')}>Component! aaaaaaaaaaaaaaaaaaaaaaa</div>;
 };
 
