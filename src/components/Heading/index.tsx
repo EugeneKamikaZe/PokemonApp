e3e0f3ffae5501b0cb2c0f24b74e6e42e663fe03
Eugene <egor_egorov_qwerty@mail.ru>
2021-05-15 23:46:32 +0300

    cu headings

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import s from "./style.module.scss";
+
+interface MyHeading {
+    tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}
+
+const Heading: React.FC<MyHeading> = ({children, tag: Tag}) => {
+    return (
+        <Tag className={s[Tag]}>{children}</Tag>
+    )
+}
+
+export default Heading;
