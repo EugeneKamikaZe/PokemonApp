5481ea4f40dcb94a93496a906a7fc464626eb8fb
Eugene <egor_egorov_qwerty@mail.ru>
2021-05-24 13:40:20 +0300

    fix with generic

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import Url from 'url'
 import getUrlWithParamsConfig from "./getUrlWithParamsConfig";
 
-async function req(endpoint: string) {
+async function req <T>(endpoint: string): Promise<T> {
     const uri = Url.format(getUrlWithParamsConfig(endpoint))
 
     return await fetch(uri).then((res) => res.json())
