fdb6434634123542e2469ed6877597044007dda3
Eugene <egor_egorov_qwerty@mail.ru>
2021-05-31 14:52:48 +0300

    fix null

--- src/utils/stringToCapitalize.test.ts
+++ src/utils/stringToCapitalize.test.ts
@@ -1,9 +1,10 @@
 import stringToCapitalize from './stringToCapitalize'
 
 describe('stringToCapitalize', () => {
-    test('Функция должна возвращать строку, начинающуюся с заглавной', () => {
+    test('Функция должна возвращать не пустую строку, начинающуюся с заглавной', () => {
         const result = stringToCapitalize('some String')
 
         expect(result).toBe('Some string')
+        expect(result).not.toBeNull()
     })
 })
